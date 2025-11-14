ad5a1ede6ba447462932184506390ad14fc3304d
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-11-14 13:27:48 -0300

    utilizando metodo provido pela SDJPA no controller

--- src/main/java/org/zzzimmer/estudoebook/controller/CategoriaController.java
+++ src/main/java/org/zzzimmer/estudoebook/controller/CategoriaController.java
@@ -4,12 +4,15 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.zzzimmer.estudoebook.domain.Categoria;
 import org.zzzimmer.estudoebook.repository.CategoriaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CategoriaController {
@@ -17,11 +20,18 @@ public class CategoriaController {
     @Autowired
     CategoriaRepository categoriaRepository;
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
     @GetMapping("/categorias")
     public List<Categoria> listar(){
+        //metodo provido pela SDJPA
         return categoriaRepository.findAll();
+    }
+
+    @GetMapping("/categorias/{id}")
+    public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        if (categoria.isPresent()){
+            return ResponseEntity.ok(categoria.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
