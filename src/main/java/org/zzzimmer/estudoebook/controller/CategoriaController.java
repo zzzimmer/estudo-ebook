d2e43fd6379ba5ab2fcefe4c42a76f260bd787ac
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-11-17 13:23:54 -0300

    feat: adiciona endpoints post, put e del em CategoriaController

--- src/main/java/org/zzzimmer/estudoebook/controller/CategoriaController.java
+++ src/main/java/org/zzzimmer/estudoebook/controller/CategoriaController.java
@@ -5,9 +5,7 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zzzimmer.estudoebook.domain.Categoria;
 import org.zzzimmer.estudoebook.repository.CategoriaRepository;
 
@@ -40,4 +38,41 @@ public class CategoriaController {
         List<Categoria> listDescricao = categoriaRepository.findByDescricao(desc);
         return ResponseEntity.ok(listDescricao);
     }
+
+    @PostMapping("/categorias")
+    public Categoria create(@RequestBody Categoria categoria){
+        return categoriaRepository.save(categoria);
+    }
+
+    @PutMapping("/categorias/{id}")
+    public ResponseEntity<Categoria> update(@RequestBody Categoria categoria,
+                                            @PathVariable Integer id){
+        if (!categoriaRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }else {
+            categoria.setId(id);
+            //setar esse id é importante porque o SDJPA verifica se existe
+            //o id na base, caso sim, executa a operação de update no banco
+            Categoria categoriaAtualizada = categoria;
+            return ResponseEntity.ok(categoriaRepository.save(categoriaAtualizada));
+        }
+    }
+
+    @DeleteMapping("/categorias/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Integer id){
+        if (!categoriaRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }else {
+            categoriaRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+//    De fato, para projetos pequenos não há tanto problema(acessar repository pelo controller,
+//    mas o ideal
+//    é separar do controller os códigos que tratam de rotinas lógicas de negócio da
+//    aplicação. Em outras palavras, deve-se tirar do controller as regras de negócio,
+//    fazendo dele um mediador entre os endpoints da aplicação e os serviços da API,
+//    assunto esse que abordaremos no tópico “Classes de serviço de domínio”
+
 }
