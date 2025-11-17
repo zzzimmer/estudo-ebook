11d9d1976f18f604a9e812f4c1fd85009b014c79
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-11-17 14:19:23 -0300

    fix: adiciona getters, setters, equals and hashcode a classe Produto

--- src/main/java/org/zzzimmer/estudoebook/domain/Produto.java
+++ src/main/java/org/zzzimmer/estudoebook/domain/Produto.java
@@ -1,8 +1,11 @@
 package org.zzzimmer.estudoebook.domain;
 
+import com.fasterxml.jackson.annotation.JacksonAnnotation;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 public class Produto {
@@ -16,4 +19,56 @@ public class Produto {
 
     @ManyToOne
     private Categoria categoria;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id) && Objects.equals(nome, produto.nome) && Objects.equals(descricao, produto.descricao) && Objects.equals(preco, produto.preco) && Objects.equals(categoria, produto.categoria);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, descricao, preco, categoria);
+    }
 }
