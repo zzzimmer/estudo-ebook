9b8cdbdd82f2a51272b75523c7169b7820b43cb6
zzzimmer <141684937+zzzimmer@users.noreply.github.com>
2025-11-24 21:17:34 -0300

    :feat: adiciona a migration V005. Prepara o banco de dados para a implementação de estoque

--- src/main/resources/db/migration/migration/V005__create__table__estoque.sql
+++ src/main/resources/db/migration/migration/V005__create__table__estoque.sql
@@ -1,8 +1,9 @@
 create table estoque(
                         produto_id int not null references produto(id),
-                        quantidade int,
-                        quantidade_maxima int not null,
-                        quantidade_minima int not null,
+                        quantidade int not null default 0,
+                        qtd_max int default 0,
+                        qtd_min int default 0,
+                        situacao ENUM('ATIVO','INATIVO','BLOQUEADO') not null DEFAULT 'ATIVO',
 
                         primary key (produto_id)
 );
